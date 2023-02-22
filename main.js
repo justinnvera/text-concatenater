0f87e9aff454644642c68a682d5cd785e586932f
Justin Vera <justinnvera@gmail.com>
2023-02-22 17:30:14 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -5,22 +5,20 @@ let inputB = document.getElementById("input-2");
 let output = document.getElementById("output");
 
 // Option Variables
-let removeWhiteSpaceOption = document.getElementById("remove-white-space-input");
-let removeEmptyLines = document.getElementById("remove-empty-lines");
-
-// Results Variables
-let outputResult;
-
-
+let removeWhiteSpaceOption = document.getElementById("remove-white-space");
+let removeEmptyLinesOption = document.getElementById("remove-empty-lines");
+let customSeperatorOption = document.getElementById("seperator-input");
 
 // ! Functions
-function combine() { 
-    // If remove white space is true 
-    if (removeWhiteSpaceOption.checked === true) {
-        outputResult = inputA.value.replace(/\s/g,'') + inputB.value.replace(/\s/g,'');
-    } else {
-        outputResult = inputA.value + inputB.value;   
+function combine () {
+    // Default output value
+    output.value = inputA.value + customSeperatorOption.value + inputB.value;
+    // If remove whitespace is true 
+    if(removeWhiteSpaceOption.checked === true) {
+        output.value = inputA.value.replace(/\s+/g, '') + customSeperatorOption.value + inputB.value.replace(/\s+/g, '');
     }
-    console.log(outputResult);
+    // If remove empty lines is true
+    if(removeEmptyLinesOption.checked === true) {
+        output.value = inputA.value.replace(/^[ \t]*[\r\n]+/gm, "") + customSeperatorOption.value + inputB.value.replace(/^[ \t]*[\r\n]+/gm, "");
+    }
 }
-
